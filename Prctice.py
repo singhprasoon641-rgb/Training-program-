256b440c583dd9a74e748394e4909a5187553caf
singhprasoon641-rgb <singhprasoon641@gmail.com>
2026-01-14 11:53:16 +0530

    my first python code

---
+++ Prctice.py
@@ -0,0 +1,95 @@
+# a=10
+# print(a)
+
+# a=int(input("enter the number"))
+# a +=5
+# print("after increasing 5",a)
+# a -=5
+# print("after decreasing 5",a)
+# a *=5
+# print("after multiplying by 5",a)
+# a /=5
+# print("after dividing by 5",a)
+# a //=5
+# print("after floor division by 5",a)
+# a %=5
+# print("after modulo by 5", a)
+# a **=5
+# print("after power by 5",a)
+
+# print positive, negative or zero
+
+# a=int(input("enter the number"))
+# if a>0:
+#     print("positive number")1
+# elif a<0:
+#     print("Negative number")
+# else:
+#     print("zero")
+
+# check eligible for vote or not
+
+# a=int(input("enter the age"))
+# if a>=18:
+#     print("eligible for vote")
+# else:
+#     print("Not eligible for vote")
+
+#     # check even or odd
+
+# a=int(input("enter the number"))
+# if a%2==0:
+#     print("even number")
+# else:
+#     print("odd number")
+
+#     # check divisible by 5 or not
+
+# a=int(input("enter the number"))
+# if a%5==0:
+#     print("Divisible by 5")
+# else:
+#     print("Not Divisible by 5")
+      
+
+# check leap year or not
+# a=int(input("enter the year"))
+# if (a%4==0 or a%100!=0) or (a%400==0):
+#     print("leap year")
+# else:
+#     print("not a leap year")
+
+# take a temperature in celsius an dprint
+
+# a=int(input("enter the temp"))
+# if a<0:
+#     print("Freezing")
+# elif a>=0 or a<=20:
+#     print("Cold")
+# elif a>=21 or a<=35:
+#     print("warm")
+# else:
+#     print("hot")
+
+
+# a=int(input("enter the number"))
+# b=int(input("enter the number"))
+# c=int(input("enter the number"))
+# if a>=35 or b>=35 or a>=35:
+#     print("pass")
+# else:
+#     print("Fail")
+
+a=input("enter the Char")
+if (a=='a' or a=='e' or a=='i' or a=='o' or a=='u') or (a=='A' or a=='E' or a=='I' or a=='O' or a=='U'):
+    print("V")
+else:
+    print("Not V") 
+
+
+
+
+
+
+
+
---
+++ list.py
@@ -0,0 +1,23 @@
+# a=[1,2,3,4,5]
+# print("first",a[0], "last",a[-1], "length",len(a))
+
+# a=[1,2,3,4,5]
+# b=sum(a)
+# print(b)
+
+# f=["Apple", "mango", "grapes","banana"]
+# f.append("papaya")
+# f.insert(0,"Apple")
+# print(f)
+
+# l=[1,2,3,4,5]
+# a=l.index(3)
+# print(a)
+
+# l=[2,3,1,6,5]
+# l.sort(reverse=True)
+# print(l)
+
+l=[2,3,1,6,5]
+l.reverse()
+print(l)
